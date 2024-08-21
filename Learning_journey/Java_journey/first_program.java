cd42e661299098d5880540fd97d8e3511d938b60
Arshatparvaes-B <108544023+Arshatparvaes-B@users.noreply.github.com>
2024-08-21 13:51:00 +0530

    Update first_program.java

--- Learning_journey/Java_journey/first_program.java
+++ Learning_journey/Java_journey/first_program.java
@@ -1,4 +1,4 @@
-public class Main {
+class first_program{
     public static void main(String[] args){
         System.out.println("Hello world! This is Java");//next line with ln
         System.out.print("Hello world! This is Arshat\n");//next line with backslash
