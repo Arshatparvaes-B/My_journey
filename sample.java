9e7a20b893b955f1f327de16dec853187c47c020
Arshatparvaes-B <108544023+Arshatparvaes-B@users.noreply.github.com>
2024-08-21 13:45:11 +0530

    Add files via upload

---
+++ sample.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world! This is Java");//next line with ln
+        System.out.print("Hello world! This is Arshat\n");//next line with backslash
+        System.out.print("Hello world! This is Parvaes");//normal print without next line
+    }
+
+}
