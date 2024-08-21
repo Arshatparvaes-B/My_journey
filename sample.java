71de336ff52b02c42837621560ac0fe085a478cb
Arshatparvaes-B <108544023+Arshatparvaes-B@users.noreply.github.com>
2024-08-21 13:46:30 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,4 +1,4 @@
-public class Main {
+class sample {
     public static void main(String[] args){
         System.out.println("Hello world! This is Java");//next line with ln
         System.out.print("Hello world! This is Arshat\n");//next line with backslash
